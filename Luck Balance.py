22b1b76c730463d1fb31028cf6905c365b88c789
dbhrockzz <dbhrockzz1@gmail.com>
2016-07-16 14:55:31 +0530

    Algorithms -> Greedy -> Luck Balance

--- Luck Balance.py
+++ Luck Balance.py
@@ -4,7 +4,7 @@ contests = []
 for i in range(n):
     contests.append(map(int,raw_input().split()))
 imp = sorted((i[0] for i in filter(lambda x:x[1]==1,contests)), key=lambda y:y,reverse=True)
-unimp = sorted((i[0] for i in filter(lambda x:x[1]==0,contests)), key=lambda y:y)
+unimp = [i[0] for i in filter(lambda x:x[1]==0,contests)]
 #print imp
 #print unimp
 luck = sum(imp[:k])
