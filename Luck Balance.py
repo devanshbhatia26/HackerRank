e12c07a9872f42ce842ea84c72da8d1c9eb2c549
dbhrockzz <dbhrockzz1@gmail.com>
2016-07-16 14:51:07 +0530

    Algortithms -> Greedy -> Luck Balance

---
+++ Luck Balance.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n,k = map(int,raw_input().split())
+contests = []
+for i in range(n):
+    contests.append(map(int,raw_input().split()))
+imp = sorted((i[0] for i in filter(lambda x:x[1]==1,contests)), key=lambda y:y,reverse=True)
+unimp = sorted((i[0] for i in filter(lambda x:x[1]==0,contests)), key=lambda y:y)
+#print imp
+#print unimp
+luck = sum(imp[:k])
+print luck+sum(unimp)-sum(imp[k:])
